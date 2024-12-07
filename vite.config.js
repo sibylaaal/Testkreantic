0dd1948ca35dc2cb992ab6e9378077aa3e11ef38
sibylaaal <bybill757@gmail.com>
2024-12-07 14:40:08 +0100

    fixed dep error

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: ['bootstrap/dist/css/bootstrap.min.css'],
+  },
   plugins: [
     vue(),
     vueDevTools(),
